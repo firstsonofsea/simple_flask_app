d245f5ef6c223a14557b5f509a54575b754626cf
unknown <firstsonofsea@yandex.ru>
2020-10-28 14:30:49 +0300

    123

--- app/routes.py
+++ app/routes.py
@@ -102,7 +102,7 @@ def otz_izm():
         r = request.json
         print(r)
         if r['type_com'] == 'DELETE':
-            otz = Otziv.query.filter_by(name=r['name'])
+            otz = Otziv.query.filter_by(name=r['name']).first()
             db.session.delete(otz)
             db.session.commit()
         if r['type_com'] == 'INSERT':
